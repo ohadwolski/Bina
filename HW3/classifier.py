7c04a53341fe59397757c94c468b35034553959c
Ohad Wolski <ohadwolski@gmail.com>
2019-01-21 21:19:16 +0200

    main + competition classifier + tests for part C

--- HW3/classifier.py
+++ HW3/classifier.py
@@ -1,11 +1,16 @@
 from hw3_utils import *
 import math
-
+import random
+import pickle
 from sklearn import svm
-
+from sklearn.ensemble import AdaBoostClassifier
+import sklearn.tree
+import sklearn.linear_model
+import csv
+import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
 
 def euclidean_distance(features_1, features_2):
-    return np.linalg.norm(features_1 - features_2) ## REMOVE BEFORE SUBMISSSION
     count = 0
     for feature_1, feature_2 in zip(features_1, features_2):
         count += (feature_1 - feature_2) ** 2
@@ -36,17 +41,17 @@ class knn_classifier(abstract_classifier):
 
 
 class knn_factory(abstract_classifier_factory):
+    name = "KNN"
 
     def __init__(self, k):
         self.k = k
+        self.name = "KNN with k=" + str(self.k)
 
     def train(self, data, labels):
         return knn_classifier(data, labels, self.k)
 
 
 
-import random
-import pickle
 FOLD_FILENAME = "ecg_fold_%d.data"
 
 
@@ -137,8 +142,7 @@ def evaluate(classifier_factory, k):
     return accuracy, error
 
 
-import sklearn.tree
-import sklearn.linear_model
+
 
 
 class tree_classifier(abstract_classifier):
@@ -159,6 +163,26 @@ class tree_factory(abstract_classifier_factory):
         tree = sklearn.tree.DecisionTreeClassifier(criterion="entropy")
         tree = tree.fit(data, labels)
         return tree_classifier(tree)
+
+
+class adaBoostClassifier(abstract_classifier):
+    def __init__(self, adaboost_clf):
+        self.clf = adaboost_clf
+
+    def classify(self, features):
+        return self.clf.predict(features.reshape(1, -1))[0]
+
+
+class adaBoostFactory(abstract_classifier_factory):
+    name = "AdaBoost Classifier"
+
+    def __init__(self):
+        pass
+
+    def train(self, data, labels):
+        clf = AdaBoostClassifier(n_estimators=100)
+        clf.fit(data, labels)
+        return adaBoostClassifier(clf)
 
 
 class svm_classifier(abstract_classifier):
@@ -183,11 +207,12 @@ class svm_factory(abstract_classifier_factory):
 
 
 class ensemble_classifier(abstract_classifier):
-    def __init__(self, knn_clf, svm_clf, tree_min_samples_clf, perceptron_clf):
+    def __init__(self, knn_clf, svm_clf, tree_min_samples_clf, adaboost_clf):
         self.knn_clf = knn_clf
         self.svm_clf = svm_clf
         self.tree_min_samples_clf = tree_min_samples_clf
-        self.perceptron_clf = perceptron_clf
+        #self.perceptron_clf = perceptron_clf
+        self.adaboost_clf = adaboost_clf
 
     def classify(self, features):
         if self.knn_clf.classify(features) == 1:
@@ -198,7 +223,7 @@ class ensemble_classifier(abstract_classifier):
                 vote += 1
             if self.tree_min_samples_clf.classify(features) == 1:
                 vote += 1
-            if self.perceptron_clf.classify(features) == 1:
+            if self.adaboost_clf.classify(features) == 1:
                 vote += 1
             if vote > 1:
                 return 1
@@ -217,9 +242,10 @@ class ensemble_factory(abstract_classifier_factory):
         #tree_clf = tree_factory().train(data, labels)
         svm_clf = svm_factory().train(data, labels)
         tree_min_samples_clf = tree_min_samples_leaf_factory().train(data, labels)
-        perceptron_clf = perceptron_factory().train(data, labels)
+        #perceptron_clf = perceptron_factory().train(data, labels)
+        adaboost_clf = adaBoostFactory().train(data, labels)
 
-        return ensemble_classifier(knn_clf, svm_clf, tree_min_samples_clf, perceptron_clf)
+        return ensemble_classifier(knn_clf, svm_clf, tree_min_samples_clf, adaboost_clf)
 
 
 
@@ -255,59 +281,156 @@ class perceptron_factory(abstract_classifier_factory):
         return perceptron_classifier(perceptron)
 
 
+## the competition classifier:
 
-###
-#### Load Features
-###train_features, train_labels, test_features = load_data()
-###
-#### 3.2
-###num_folds = 2
-###
-#### run only once:
-###split_crosscheck_groups((train_features, train_labels), num_folds)
-####
-###
-#### 3.5
-###accuracy = {}
-###error = {}
-###for k in [1, 3, 5, 7, 13]:
-###    print("K=%d" % k)
-###    knn = knn_factory(k)
-###    accuracy[k], error[k] = evaluate(knn, num_folds)
-###    print("K=%d:" % k, "Accuracy=%f" % accuracy[k], "Error=%f" % error[k])
-###    print()
-###
-#### 3.5.1
-###import csv
-###with open("experiments6.csv", 'w') as fd:
-###    csv_writer = csv.writer(fd)
-###    for k in accuracy:
-###        csv_writer.writerow([k, accuracy[k], error[k]])
-###
-#### 3.5.2
-###import matplotlib.pyplot as plt
-###plt.figure()
-###plt.title("Accuracy as function of K")
-###plt.xlabel("K")
-###plt.ylabel("Accuracy")
-###plt.plot(accuracy.keys(), accuracy.values())
-###plt.show()
-###
-###
-# 7
+def scale_data(train_features, train_labels):
+
+    threshold = 0.04
+    train_features_scaled = []
+    train_labels_scaled = []
+
+    for i, features in enumerate(train_features):
+        # check all samples greater than threshold
+        indexes_bigger_than_threshold = np.where(np.abs(features) > threshold)[0]
+        if len(indexes_bigger_than_threshold) == 0:
+            continue
+
+        # cut the samples and use interp1d from scipy library
+        cut_sample_index = indexes_bigger_than_threshold[-1]
+        cut_sample_value = features[:cut_sample_index]
+        interpolation_function = interp1d(np.linspace(0, 1, len(cut_sample_value)), cut_sample_value)
+        interpolation_values = interpolation_function(np.linspace(0, 1, len(features)))
+
+        train_features_scaled.append(interpolation_values)
+        train_labels_scaled.append(train_labels[i])
+
+    train_labels_scaled = np.array(train_labels_scaled)
+    train_features_scaled = np.array(train_features_scaled)
+
+    return train_features_scaled, train_labels_scaled
 
 
-###
-###accuracy = {}
-###error = {}
-###for i, factory in enumerate([tree_factory, perceptron_factory]):
-###    accuracy[i], error[i] = evaluate(factory(), num_folds)
-###    print("%s:" % factory.name, "Accuracy=%f" % accuracy[i], "Error=%f" % error[i])
-###    print()
-###import csv
-###with open("experiments12.csv", 'w') as fd:
-###    csv_writer = csv.writer(fd)
-###    for k in accuracy:
-###        csv_writer.writerow([k+1, accuracy[k], error[k]])
-###
-###
+def scale_test(test_features):
+
+    threshold = 0.04
+    train_features_scaled = []
+    test_features = [test_features]
+
+    for i, features in enumerate(test_features):
+        # check all samples greater than threshold
+        indexes_bigger_than_threshold = np.where(np.abs(features) > threshold)[0]
+        if len(indexes_bigger_than_threshold) == 0:
+            train_features_scaled = test_features[0]
+            break
+
+        # cut the samples and use interp1d from scipy library
+        cut_sample_index = indexes_bigger_than_threshold[-1]
+        cut_sample_value = features[:cut_sample_index]
+        interpolation_function = interp1d(np.linspace(0, 1, len(cut_sample_value)), cut_sample_value)
+        interpolation_values = interpolation_function(np.linspace(0, 1, len(features)))
+
+        train_features_scaled = interpolation_values
+
+    train_features_scaled = np.array(train_features_scaled)
+
+    return train_features_scaled
+
+
+class knn_with_preprocessing_classifier(abstract_classifier):
+    def __init__(self, data, labels, k=10):
+        self.data = data
+        self.labels = labels
+        self.k = k
+
+    def classify(self, features):
+        assert not (len(features) == 0)
+        scaled_features = scale_test(features)
+        assert not (len(scaled_features) == 0)
+        features = scaled_features
+
+        distances = []
+        for known_features in self.data:
+            distances.append(euclidean_distance(known_features, features))
+
+        votes = {True: 0, False: 0}
+        for _ in range(self.k):
+            nn_idx = distances.index(min(distances))
+            votes[self.labels[nn_idx]] += 1
+            distances[nn_idx] = float('inf')
+
+        if votes[True] > votes[False]:
+            return 1
+        else:
+            return 0
+
+
+class knn_with_preprocessing_factory(abstract_classifier_factory):
+    name = "KNN with pre processing"
+
+    def __init__(self, k):
+        self.k = k
+        self.name = self.name + " with k=" + str(self.k)
+
+    def train(self, data, labels):
+        train_features_scaled, train_labels_scaled = scale_data(data, labels)
+        return knn_with_preprocessing_classifier(train_features_scaled, train_labels_scaled, self.k)
+
+
+if __name__ == '__main__':
+    # Load Features
+    train_features, train_labels, test_features = load_data()
+
+    # 3.2
+    num_folds = 2
+
+    # run only once:
+    split_crosscheck_groups((train_features, train_labels), num_folds)
+    #
+
+    # 3.5
+    accuracy = {}
+    error = {}
+    for k in [1, 3, 5, 7, 13]:
+        print("K=%d" % k)
+        knn = knn_factory(k)
+        accuracy[k], error[k] = evaluate(knn, num_folds)
+        print("K=%d:" % k, "Accuracy=%f" % accuracy[k], "Error=%f" % error[k])
+        print()
+
+    # 3.5.1
+
+    with open("experiments6.csv", 'w') as fd:
+        csv_writer = csv.writer(fd)
+        for k in accuracy:
+            csv_writer.writerow([k, accuracy[k], error[k]])
+
+    # 3.5.2
+
+    plt.figure()
+    plt.title("Accuracy as function of K")
+    plt.xlabel("K")
+    plt.ylabel("Accuracy")
+    plt.plot(accuracy.keys(), accuracy.values())
+    plt.show()
+
+    # 7
+    accuracy = {}
+    error = {}
+    for i, factory in enumerate([tree_factory, perceptron_factory]):
+        accuracy[i], error[i] = evaluate(factory(), num_folds)
+        print("%s:" % factory.name, "Accuracy=%f" % accuracy[i], "Error=%f" % error[i])
+        print()
+    with open("experiments12.csv", 'w') as fd:
+        csv_writer = csv.writer(fd)
+        for k in accuracy:
+            csv_writer.writerow([k+1, accuracy[k], error[k]])
+
+
+    # Creating the prediction for the test:
+    train_features, train_labels, test_features = load_data()
+    factory = knn_with_preprocessing_factory(1)
+    classifier = factory.train(train_features, train_labels)
+    results = []
+    for test_feature in test_features:
+        results.append(classifier.classify(test_feature))
+    write_prediction(results)
--- HW3/competition_classifier.py
+++ HW3/competition_classifier.py
@@ -1,12 +1,12 @@
 from statistics import mean, pvariance
-
+from scipy.interpolate import interp1d
 from sklearn import *
 from sklearn.decomposition import PCA
 from sklearn.feature_selection import SelectKBest, chi2
-
-from HW3.hw3_utils import *
+from hw3_utils import *
 import math
-from HW3.classifier import *
+from classifier import *
+import matplotlib.pyplot as plt
 
 def normalize_data():
     train_features, train_labels, test_features = load_data()
@@ -24,7 +24,7 @@ def evaluate_folds(classifier_factory, folds):
     return accuracy, error
 
 
-# run main for part 3:
+# run tests for part 3:
 ##
 ##FOLD_FILENAME = "ecg_fold_%d_normalized.data"
 ##
@@ -83,5 +83,76 @@ def evaluate_folds(classifier_factory, folds):
 ##print("%s:" % tree_factory.name, "Accuracy=%f" % accuracy, "Error=%f" % error)
 
 ## using knn with k=1 and ensemble of classifiers:
-accuracy, error = evaluate(ensemble_factory(), 5)
-print("%s:" % tree_factory.name, "Accuracy=%f" % accuracy, "Error=%f" % error)
+##accuracy, error = evaluate(ensemble_factory(), 5)
+##print("%s:" % tree_factory.name, "Accuracy=%f" % accuracy, "Error=%f" % error)
+
+
+## using adaBoost with 100 estimators of stumps:
+#accuracy, error = evaluate(adaBoostFactory(), 5)
+#print("%s:" % tree_factory.name, "Accuracy=%f" % accuracy, "Error=%f" % error)
+
+
+## using reduced data with knn:
+#accuracy, error = evaluate(knn_reduced_samples_factory(1), 5)
+#print("%s:" % tree_factory.name, "Accuracy=%f" % accuracy, "Error=%f" % error)
+
+
+
+#train_features, train_labels, test_features = load_data()
+#train_labels = np.array(train_labels)
+#train_features = np.array(train_features)
+
+#split_crosscheck_groups((train_features, train_labels), 5)
+
+## Test for viewing features as continuous graph:
+##plt.figure()
+##plt.plot((train_features[train_labels == True]).T)
+##plt.show()
+##plt.figure()
+##plt.plot((train_features[train_labels == False]).T)
+##plt.show()
+
+## Removing samples that are "dead", that do not go over a certain threshold
+## Scaling all other samples so they fit in 187 samples
+
+
+##train_features_scaled, train_labels_scaled = scale_data(train_features, train_labels)
+##plt.figure()
+##plt.plot((train_features_scaled[train_labels_scaled == True]).T)
+##plt.show()
+##plt.figure()
+##plt.plot((train_features_scaled[train_labels_scaled == False]).T)
+##plt.show()
+
+
+
+
+## checking knn with preprocessing of data, for different k':
+
+
+##factory = knn_with_preprocessing_factory(1)
+##accuracy, error = evaluate(factory, 5)
+##print("%s:" % factory.name, "Accuracy=%f" % accuracy, "Error=%f" % error)
+##factory = knn_with_preprocessing_factory(3)
+##accuracy, error = evaluate(factory, 5)
+##print("%s:" % factory.name, "Accuracy=%f" % accuracy, "Error=%f" % error)
+##factory = knn_with_preprocessing_factory(5)
+##accuracy, error = evaluate(factory, 5)
+##print("%s:" % factory.name, "Accuracy=%f" % accuracy, "Error=%f" % error)
+##factory = knn_with_preprocessing_factory(7)
+##accuracy, error = evaluate(factory, 5)
+##print("%s:" % factory.name, "Accuracy=%f" % accuracy, "Error=%f" % error)
+##factory = knn_with_preprocessing_factory(13)
+##accuracy, error = evaluate(factory, 5)
+##print("%s:" % factory.name, "Accuracy=%f" % accuracy, "Error=%f" % error)
+
+
+
+
+
+
+
+
+
+
+
